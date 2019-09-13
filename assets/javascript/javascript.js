ce2c0e926512674343b8beb309ecf57cbd79896c
Gregory Henderson <gh_03@yahoo.com>
2019-09-13 13:55:24 -0500

    updating the ID issue in the HTML and JS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,11 @@
-/ Steps to complete:
+// Steps to complete:
 
 // 1. Initialize Firebase
 // 2. Create button for adding new employees - then update the html + update the database
 // 3. Create a way to retrieve employees from the employee database.
 // 4. Create a way to calculate the months worked. Using difference between start and current time.
 //    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
+// 5. Calculate Total billed  
 
 // 1. Initialize Firebase
 var config = {
